29b43bb22b74865054082c38f989d0ef6b8568de
Esense <klg-hunter@mail.ru>
2022-03-09 12:02:14 +0200

    My first commit. Create connection to GitHub and create Lesson1

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,48 @@
+﻿decimal result = 0; //результат падает сюда
+string[] act = { "+", "-", "/", "*" };
+string value;
+bool msgerror;
+Console.WriteLine("Введите первое число : ");
+msgerror = decimal.TryParse(Console.ReadLine(), out decimal a);
+    while (!msgerror)
+        {
+          Console.WriteLine("Неверно введено первое число, повторите : ");
+          msgerror = decimal.TryParse(Console.ReadLine(), out a);
+        }
+ Console.WriteLine("Введите второе число : ");
+ msgerror = decimal.TryParse(Console.ReadLine(), out decimal b);
+    while (!msgerror)
+        {
+          Console.WriteLine("Неверно введено второе число, повторите : ");
+          msgerror = decimal.TryParse(Console.ReadLine(), out b);
+        }
+Console.WriteLine("Введите действие, где '*' - умножение, '/' - деление, '-' - вычитание, '+' - сложение :");
+msgerror = act.Contains(value = Console.ReadLine());
+
+switch (value) {
+    case "*": result = a * b; break;
+    case "/": if (b != 0)
+        {
+            result = a / b;
+            break;
+        }
+        else
+        { 
+            while (b == 0)
+            {
+                Console.WriteLine("Вы пытаетесь поделить на ноль, введите другое число : ");
+                msgerror = decimal.TryParse(Console.ReadLine(), out b);
+                while (!msgerror)
+                {
+                    Console.WriteLine("Неверно введено второе число, повторите : ");
+                    msgerror = decimal.TryParse(Console.ReadLine(), out b);
+                }
+            }
+            result = a / b;
+            break;
+        }
+    case "+": result = a + b; break;
+    case "-": result = a - b; break;
+}
+
+Console.WriteLine("{0} {1} {2} = {3}", a, value, b, result);            
